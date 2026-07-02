6908db61b86ba980b364d71852bce80214528652
Austine <austinemark254@gmail.com>
2026-07-02 18:42:27 +0300

    removing unwanted

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import adapter from '@sveltejs/adapter-cloudflare';
-=======
 import adapter from '@sveltejs/adapter-static';
->>>>>>> beea415 (changing structure)
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-<<<<<<< HEAD
-	kit: { adapter: adapter() }
-=======
 
 	kit: {
 		adapter: adapter({
@@ -21,11 +14,9 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			// REPL_YOUR_REPO_NAME: Put your exact GitHub repository name below
 			base: process.env.NODE_ENV === 'production' ? '/portfolio' : ''
 		}
 	}
->>>>>>> beea415 (changing structure)
 };
 
 export default config;
